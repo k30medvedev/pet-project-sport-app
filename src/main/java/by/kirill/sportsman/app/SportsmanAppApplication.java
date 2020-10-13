7c50e40f7d514a48284461a454d6ac5fc30ad93a
Kirill Medvedev <k30medvedev@gmail.com>
2020-10-13 22:20:49 +0300

    add mvn

---
+++ src/main/java/by/kirill/sportsman/app/SportsmanAppApplication.java
@@ -0,0 +1,14 @@
+package by.kirill.sportsman.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsmanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsmanAppApplication.class, args);
+	}
+
+}
+
